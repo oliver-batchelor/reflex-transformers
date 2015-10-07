68b0ccc28ccecc6e05a66f3627366515c8331f78
Oliver Batchelor <saulzar@gmail.com>
2015-10-07 20:30:27 +1300

    Fix shadowing

--- src/Reflex/Monad.hs
+++ src/Reflex/Monad.hs
@@ -53,8 +53,8 @@ holdM initial e = holdDyn' =<< switchM (Updated initial e)
 
 withIds :: (MonadReflex t m) => [a] -> Event t [a] -> m (Map Int a, Event t (Map Int a))
 withIds initial added = do
-  count <- current <$> (foldDyn (+) (genericLength initial)  (genericLength <$> added))
-  return (zipFrom 0 initial, attachWith zipFrom count added)
+  total <- current <$> (foldDyn (+) (genericLength initial)  (genericLength <$> added))
+  return (zipFrom 0 initial, attachWith zipFrom total added)
     where
       zipFrom n = Map.fromList . zip [n..] 
     
